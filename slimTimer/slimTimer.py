57927e5805deacc1512772df93596165579064ff
bhornung <bh13158@bristol.ac.uk>
2018-04-15 10:47:52 +0100

    Add todict(). Fix number of bugs.

--- slimTimer/slimTimer.py
+++ slimTimer/slimTimer.py
@@ -5,16 +5,76 @@ import numpy as np
 class SlimTimer():
     """
     Bare to bones timer utlity
+
+    Attributes
+       self.func (callable) : function to time
+       self.n_runs (int) : number of runs with a set of parameters.
+       self.tag (str) : string to tag the timer
+       self.verbosity (int) : output verbosity level
+
+       self.runtimes (np.ndarray) : list of run times
+       self.tmean (float) : mean run time
+       self.tstdev (float) : standard deviation of run times
+
+    Methods:
+      measure() : runs the function self.n_runs times
+
+      set_func_args(*args, **kwargs) : sets the function arguments. Make sure *args are passed in the right order.
+
+      set_func(func) : sets the function to be measured.
+
+      to_dict(with_tag = False) : export the public attributes to a dict. 
+        Parameters:
+          with_tag (bool) : add tag to dict keys. Default False.
+        Returns :
+          res_dict = {'[self.tag_]runtimes' : self.run_times,
+                      '[self.tag_]tmean' : self.tmean,
+                      '[self.tag_]tstdev' : self.tstdev}
     """
-    def __init__(self, n_runs = 10):
+    def __init__(self,  func = None, n_runs = 10, tag = "", verbosity = 0):
         """
         Creates an instance of the timer.
         Parameters:
+          func (callable) : function to time
           n_runs (int) : number of runs with a set of parameters.
+          tag (str) : string to tag the timer
+          verbosity (int) : output verbosity level
         """
-        self.n_runs = n_runs
+
+# add function
+        if not callable(func):
+          raise ValueError("func must be callable")
+        else:
+          self.set_func(func)
+
+# set number of runs
+        self._set_n_runs(n_runs)
+
+# set function arguments
+        self._func_args = None
+        self._func_kwargs = None
+
+# initialise timer
         self._set_runtimes()
         self.__hasrun = False
+
+# add tag
+        if tag == "":
+          self.tag = func.__name__
+        else:
+          self.tag = str(tag)
+
+        self.verbosity = verbosity
+
+    @property
+    def run_times(self):
+      """
+      np.ndarray of type np.float to store the run times.
+      """
+      if self.__hasrun:
+        return self._run_times
+      else:
+        raise ValueError("Cannot report unmesured times.")
 
     @property 
     def tmean(self):
@@ -30,26 +90,18 @@ class SlimTimer():
       else:
         raise ValueError("Cannot report unmesured times.")
 
-    @property
-    def run_times(self):
-      """
-      np.ndarray of type np.float to store the run times.
-      """
-      if self.__hasrun:
-        return self._run_times
-      else:
-        raise ValueError("Cannot report unmesured times.")
-
     def measure(self):
         """
         Performs n_runs runs. Calculates the mean run time and its standard deviation.
         """
 # --- perform repeated runs
         for i_run in range(self.n_runs):
-          print("Run {0} / {1} ...".format(i_run, self.n_runs), end = '')
+          if self.verbosity > 0:
+            print("Run {0} / {1} ...".format(i_run, self.n_runs), end = '')
           tdelta = self._timed_execute()
           self._run_times[i_run] = tdelta
-          print()
+          if self.verbosity == 2:
+            print(tdelta)
 
 # calculate mean
         self._tmean = np.mean(self._run_times)
@@ -71,15 +123,34 @@ class SlimTimer():
         Parameters:
           func (callable) : function to be timed
         """
-        self._func = func
+        if callable(func):
+            self._func = func
+        else:
+            raise TypeError("'func should be callable'")
 
-    def set_n_runs(self, n_runs):
+    def to_dict(self, with_tag = False):
+      """
+      Returns the individual, mean and error of the timings as a dictionary.
+      Parameters:
+        with_tag (Bool) : tag the dictionary with the timer's tag. Default False.
+      """
+
+      res_dict = {'runtimes' : self.run_times,
+                  'tmean' : self.tmean,
+                  'tstdev' : self.tstdev}
+
+      if with_tag:
+        res_dict = {"{0}_{1}".format(self.tag, _k) : _v for _k, _v in res_dict.items()}
+
+      return res_dict
+
+    def _set_n_runs(self, n_runs):
       """
       Sets the number of runs.
       Parameters:
         n_runs (int) : number of runs
       """
-      if not isinstance(n_runs, type) or n_runs < 1:
+      if not isinstance(n_runs, int) or n_runs < 1:
         raise ValueError("'n_runs' must be a positive integer.")
 
       self.n_runs = n_runs
@@ -90,17 +161,6 @@ class SlimTimer():
 # block acces to results
       self.__hasrun = False
 
-    def to_dict(self):
-      """
-      Returns the individual, mean and error of the timings as a dictionary.
-      """
-
-      res_dict = {'runtimes' : self.run_times,
-                  'tmean' : self.tmean,
-                  'tstdev' : self.tstdev}
-
-      return res_dict
-
     def _set_runtimes(self):
       """
       Sets the array to store the run times.
@@ -109,7 +169,7 @@ class SlimTimer():
 
     def _timed_execute(self):
         """
-        Executes function once.
+        Executes the function once.
         Returns:
           tdelta (float) : time needed to execute the function.
         """
@@ -120,4 +180,3 @@ class SlimTimer():
         tdelta = tend - tstart
 
         return tdelta
-
